e34e2a296be753d5b233364e68769021aa920491
Elmer Mendoza <mdelefm@gmail.com>
2021-09-26 12:48:46 -0600

    navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from "../components/Hero"
+
+const Home = (props) => {
+    console.log(Hero.defaultProps)
+    return (
+        <Hero/>
+    )
+}
+
+export default Home
+
